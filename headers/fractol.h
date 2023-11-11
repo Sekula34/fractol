ffb0c73b4d518f45dc1d8cf07af1856b85798f29
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-11 16:43:54 +0100

    added all libraries i will need in header

--- headers/fractol.h
+++ headers/fractol.h
@@ -1,6 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 16:42:56 by fseles            #+#    #+#             */
+/*   Updated: 2023/11/11 16:42:58 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
 # include <mlx.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <sys/types.h>
 # include <X11/X.h>
 # include "../libft/libft.h"
 
