870649cc1fdc4c5cbbd78e5afe143726d4be6415
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-13 18:25:37 +0100

    funchek

--- sources/mandelbrot.c
+++ sources/mandelbrot.c
@@ -13,38 +13,40 @@
 #include "../headers/fractol.h"
 
 
-void closing_function(void *param)
+int	closing_function(void *param)
 {
 	t_fractol *param1;
+
 	param1 = (t_fractol *)param;
 	mlx_destroy_image(param1->mlx_ptr, param1->img.img);
 	mlx_destroy_window(param1->mlx_ptr, param1->win_ptr);
 	mlx_destroy_display(param1->mlx_ptr);
 	free(param1->mlx_ptr);
 	exit(EXIT_SUCCESS);
+	return (0);
 }
 
 int closef(int keycode, void *param)
 {
 	if (keycode == 65307)
-	{
 		closing_function(param);
-	}
-	ft_printf("Stisnuta tipka : %d\n", keycode);
 	return (1);
 }
+
+
 
 void	mandelbrot()
 {
 	t_fractol data;
 
-	ft_printf("Uso sam ");
 	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		exit(EXIT_FAILURE);
 	data.win_ptr = mlx_new_window(data.mlx_ptr, WIDTHX, HEIGHTY, "Prozor stare majke");
 	data.img.img = mlx_new_image(data.mlx_ptr, WIDTHX, HEIGHTY);
 	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pixel, &data.img.line_length, &data.img.endian);
 	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img.img, 0, 0);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, closef, &data);
+	mlx_hook(data.win_ptr, DestroyNotify, NoEventMask,closing_function, &data);
 	mlx_loop(data.mlx_ptr);
-
 }
