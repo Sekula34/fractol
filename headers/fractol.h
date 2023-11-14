55ce95f9754902b0705d47f548ee08847602be03
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-14 14:12:08 +0100

    headear

--- headers/fractol.h
+++ headers/fractol.h
@@ -26,10 +26,10 @@
 
 # define HEIGHTY 480
 # define WIDTHX 640
-# define COORDINATE_X_MAX 2
+# define COORDINATE_X_MAX 0.75
 # define COORDINATE_X_MIN -2
-# define COORDINATE_Y_MAX 2
-# define COORDINATE_Y_MIN -2
+# define COORDINATE_Y_MAX 1.25
+# define COORDINATE_Y_MIN -1.25
 # define MAX_ITERATION 1000
 
 typedef struct s_data{
