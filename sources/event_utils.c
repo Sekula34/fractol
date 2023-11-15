f184b6dee4b1e70af9dc793f7baa509dec8c6f93
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-15 17:29:36 +0100

    norma event

--- sources/event_utils.c
+++ sources/event_utils.c
@@ -16,12 +16,12 @@
 //zoom in by 10% if option is 1,
 //zoom out by 10% if option is else
 //zooming is done by rescaling max and min x and y axis 
-void zooming(t_fractol *data, int option, int x, int y)
+void	zooming(t_fractol *data, int option, int x, int y)
 {
-	double new_x_diff;
-	double new_y_diff;
-	double x_center;
-	double y_center;
+	double	new_x_diff;
+	double	new_y_diff;
+	double	x_center;
+	double	y_center;
 
 	new_x_diff = (data->coordinate_x_max - data->coordinate_x_min);
 	new_y_diff = (data->coordinate_y_max - data->coordinate_y_min);
@@ -30,7 +30,7 @@ void zooming(t_fractol *data, int option, int x, int y)
 		new_x_diff = (data->coordinate_x_max - data->coordinate_x_min) * 0.9;
 		new_y_diff = (data->coordinate_y_max - data->coordinate_y_min) * 0.9;
 	}
-	else if(option == 2)
+	else if (option == 2)
 	{
 		new_x_diff = (data->coordinate_x_max - data->coordinate_x_min) * 1.1;
 		new_y_diff = (data->coordinate_y_max - data->coordinate_y_min) * 1.1;
@@ -62,24 +62,24 @@ int	closing_function(void *param)
 //fucntion that depending od option moves coordinate system
 //aka moving picture with arrrows
 //1 up 2 down, 3 right 4 left
-void moving_arr(t_fractol *data, int option)
+void	moving_arr(t_fractol *data, int option)
 {
-	if(option == 1)
+	if (option == 1)
 	{
 		data->coordinate_y_max += 0.1;
 		data->coordinate_y_min += 0.1;
 	}
-	else if(option == 2)
+	else if (option == 2)
 	{
 		data->coordinate_y_max -= 0.1;
 		data->coordinate_y_min -= 0.1;
 	}
-	else if(option == 3)
+	else if (option == 3)
 	{
 		data->coordinate_x_max += 0.1;
 		data->coordinate_x_min += 0.1;
 	}
-	else if(option == 4)
+	else if (option == 4)
 	{
 		data->coordinate_x_max -= 0.1;
 		data->coordinate_x_min -= 0.1;
