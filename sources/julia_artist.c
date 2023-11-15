6d4b894e0651bdd1f5b830613a94a5f1168f5716
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-15 17:36:44 +0100

    julia norma

--- sources/julia_artist.c
+++ sources/julia_artist.c
@@ -10,31 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../headers/fractol.h"
+#include "../headers/fractol.h"
 
-
-static int is_true_j(t_pixel pixel, double r)
+static int	is_true_j(t_pixel pixel, double r)
 {
-	double zx_pow;
-	double zy_pow;
-	double r_pow;
-	int value;
+	double	zx_pow;
+	double	zy_pow;
+	double	r_pow;
+	int		value;
 
 	zx_pow = pixel.coordinate_x * pixel.coordinate_x;
 	zy_pow = pixel.coordinate_y * pixel.coordinate_y;
 	r_pow = r * r;
 	value = 0;
-	if(pixel.iteration < pixel.max_iteration)
+	if (pixel.iteration < pixel.max_iteration)
 		value = 1;
-	if((zx_pow + zy_pow < r_pow) && value)
+	if ((zx_pow + zy_pow < r_pow) && value)
 		return (1);
 	return (0);
-
 }
 
-static void data_initilizatorj(t_pixel *pixel, int x, int y, t_fractol *data)
+static void	data_initilizatorj(t_pixel *pixel, int x, int y, t_fractol *data)
 {
-	if(pixel == NULL)
+	if (pixel == NULL)
 		return ;
 	pixel->screen_x = x;
 	pixel->screen_y = y;
@@ -42,35 +40,36 @@ static void data_initilizatorj(t_pixel *pixel, int x, int y, t_fractol *data)
 	pixel->coordinate_y = get_y_from_heighty(y, data);
 	pixel->iteration = 0;
 	pixel->max_iteration = MAX_ITERATION;
-	data->julia_R = 2;
+	data->julia_r = 2;
 	pixel->xtemp = 0.0;
 }
 
-static void every_pixel_julia(int x, int y, t_fractol *data)
+static void	every_pixel_julia(int x, int y, t_fractol *data)
 {
-	t_pixel pixel;
-	double x_pow;
-	double y_pow;
-	double zxzy;
-	
+	t_pixel	pixel;
+	double	x_pow;
+	double	y_pow;
+	double	zxzy;
+
 	data_initilizatorj(&pixel, x, y, data);
-	while (is_true_j(pixel, data->julia_R))
+	while (is_true_j(pixel, data->julia_r))
 	{
 		x_pow = pixel.coordinate_x * pixel.coordinate_x;
 		y_pow = pixel.coordinate_y * pixel.coordinate_y;
 		zxzy = pixel.coordinate_x * pixel.coordinate_y;
 		pixel.xtemp = x_pow - y_pow;
-		pixel.coordinate_y= 2*zxzy + data->julia_cy;
+		pixel.coordinate_y = 2 * zxzy + data->julia_cy;
 		pixel.coordinate_x = pixel.xtemp + data->julia_cx;
-		pixel.iteration = pixel.iteration + 1;   
+		pixel.iteration = pixel.iteration + 1;
 	}
-	if(pixel.iteration  == pixel.max_iteration)
+	if (pixel.iteration == pixel.max_iteration)
 		my_mlx_pixel_put(&(data->img), x, y, 0x00000000);
 	else
-		my_mlx_pixel_put(&(data->img),x, y, (0x00A0A0A0/pixel.max_iteration) * pixel.iteration);
+		my_mlx_pixel_put(&(data->img), x, y,
+			(0x00A0A0A0 / pixel.max_iteration) * pixel.iteration);
 }
 
-void julia_artist(t_fractol *fractol)
+void	julia_artist(t_fractol *fractol)
 {
 	int	x;
 	int	y;
